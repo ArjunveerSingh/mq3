c021365bc1fd82678823d82d8622d8845ce29d12
ArjunveerSingh <76784986+ArjunveerSingh@users.noreply.github.com>
2022-05-12 00:05:14 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+function login(){
+    p1=document.getElementById("u1").value;
+    p2=document.getElementById("u2").value;
+    localStorage.setItem("p1",p1);
+    localStorage.setItem("p2",p2);
+    window.location="qgp.html";
+}
