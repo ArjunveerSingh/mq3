f6662892a4e9a4a10ef7faf656decebf268b2749
ArjunveerSingh <76784986+ArjunveerSingh@users.noreply.github.com>
2022-05-12 00:16:33 +0530

    Update qgp.js

--- qgp.js
+++ qgp.js
@@ -2,7 +2,8 @@ function send(){
     n1=document.getElementById("n1").value;
     n2=document.getElementById("n2").value;
     a=parseInt(n1)*parseInt(n2);
-    qn="<h4>"+n1+"X"+n2+"<h4>";
+    q=document.getElementById.innerHTML="question"
+    qn=q+n1+"X"+n2+"q;
 i="<br>Answer:<input type'text' id='input_check_box'>";
 cb = "<br><br><button class='btn btn-info' onclick="check();">Check</button>";
 row=qn+i+cb;
